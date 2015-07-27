1e77dd1eb1292bd3993dd3b58cbf1a95d11db11b
Rohan Paul <rohanpaul2@gmail.com>
2015-07-27 11:10:45 +0530

    .fixing .gitignore file

--- testing-code-3.js
+++
@@ -1,7 +0,0 @@
-var coinFace = Math.floor(Math.random() *4);
-
-while(coinFace === 0){
-    console.log("Heads! Flipping again...");
-    var coinFace = Math.floor(Math.random() * 2);
-}
-console.log("Tails! Done flipping.");
