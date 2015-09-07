574658d13ba0d30a449fd4ea1d753dcc809a4b72
Rohan Paul <rohanpaul2@gmail.com>
2015-09-08 04:29:13 +0530

    next solution

--- bonfire/basic-algorithm-scripting/06-title_case_a_sentence.js
+++ bonfire/basic-algorithm-scripting/06-title_case_a_sentence.js
@@ -7,4 +7,15 @@ String.prototype.toTitleCase = function()
         function($1) { return $1.toUpperCase(); });
 }
 
+/*A) /\s(.)/g  - Captures any single character after a space.
+
+ /^(.) - Captures the very first character of the whole string. i.e. this is input beginning specifier and capture whatever character is exactly after it.
+
+B) $n represents the nth capture group of the regular expression. $1 is the first group from my regular expression, $2 is the second.
+
+C) $1 regular expression property can be used as a function argument, can be used inside the function and is updated internally by the replace method after the function executes.
+
+D) So, in the above, I am turning the entire string into lower case and then each regexp match group to upper case using replace function.
+*/
+
 console.log(("I'm of little tea pot").toTitleCase());
--- bonfire/basic-algorithm-scripting/07-return_largest_numbers_in_arrays.js
+++ bonfire/basic-algorithm-scripting/07-return_largest_numbers_in_arrays.js
@@ -1,9 +1,17 @@
 /*Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays. Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i] . If you are writing your own Chai.js tests, be sure to use a deep equal statement instead of an equal statement when comparing arrays.*/
 
 
-function largestOfFour(arr) {
-    // You can do this!
-    return arr;
+function largestOfFour(givenArray) {
+    var largestNum = [];
+    for (var i = 0; i < givenArray.length; i++) {
+        var subArray = givenArray[i];
+          var largest = Math.max.apply(Math, subArray);
+           largestNum.push(largest);
+
+    }
+    return largestNum;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
