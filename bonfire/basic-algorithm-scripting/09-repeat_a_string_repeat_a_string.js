0f904031075ed7a096c793493552997625ed576c
Rohan Paul <rohanpaul2@gmail.com>
2015-09-09 07:39:10 +0530

    small change

--- bonfire/basic-algorithm-scripting/09-repeat_a_string_repeat_a_string.js
+++ bonfire/basic-algorithm-scripting/09-repeat_a_string_repeat_a_string.js
@@ -1,9 +1,14 @@
-/*Repeat a given string (first argument) n times (second argument). Return an empty string if n is a negative number.*/
+/*Repeat a given string (first argument) n times (second argument). Return an empty string if n is a negative number*/
 
-
-function repeat(str, num) {
-    // repeat after me
-    return str;
+function repeat (str, num) {
+    repeatedString = '';
+    if (num <= 0) {
+        return repeatedString;
+    }
+    for (var i = 0; i < num; i++ ) {
+        repeatedString += str;
+    }
+    return repeatedString;
 }
 
-repeat('abc', 3);
+console.log(repeat('abc', 3));
