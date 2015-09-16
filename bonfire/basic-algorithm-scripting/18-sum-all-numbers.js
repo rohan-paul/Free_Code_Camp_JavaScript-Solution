aee6015b04202db86a11d587a30570f49672afe7
Rohan Paul <rohanpaul2@gmail.com>
2015-09-16 17:16:22 +0530

    comments on code

--- bonfire/basic-algorithm-scripting/18-sum-all-numbers.js
+++ bonfire/basic-algorithm-scripting/18-sum-all-numbers.js
@@ -5,18 +5,25 @@ We'll pass you an array of two numbers. Return the sum of those two numbers and 
 The lowest number will not always come first.*/
 
 function sumAll(arr) {
-    var MaxNumberFromOriginalArray = function (arr) {
+    var getMaxNumberFromOriginalArray = function (arr) {
         return Math.max.apply(null, arr);
     };
 
-    var MinNumberFromOriginalArray = function (arr) {
+    var getMinNumberFromOriginalArray = function (arr) {
         return Math.min.apply(null, arr);
     };
 
-    // If I don't use the apply method, the final result will only addup the 2 given elements of the original array.
+    /* My interpretation of using the apply() in the above -
+    A) If I don't use the apply method, or use the apply method without "null" as its first argument, the final result will only addup the 2 given elements of the original array.
 
-    var secondMaxNumber = MaxNumberFromOriginalArray(arr) - 1;
-    var secondMinNumber = MinNumberFromOriginalArray(arr) + 1;
+     Now, both call() and apply() are methods we can use to assign the this pointer for the duration of a method invocation
+
+    B) The apply() method invokes the function Math.max() and uses its first parameter as the <this> pointer inside the body of the function. In other words - we've told the runtime what object to reference as <this> while executing inside of function Math.max
+
+    C) And when null or undefined is supplied as the receiver to call() or apply(), the global object is used as the value for receiver instead, as the value of this can never be null or undefined when a function is called. And here the global object is the finnal value of "arr".  */
+
+    var secondMaxNumber = getMaxNumberFromOriginalArray(arr) - 1;
+    var secondMinNumber = getMinNumberFromOriginalArray(arr) + 1;
 
     // Now to the original array I have to push all the other numbers in between the max and min numbers, so I have the final array containing the entire series of numbers from max to min.
 
