bfd6c9bd613d4429b11c7f6489f5e2d0ae18ef7d
Rohan Paul <rohanpaul2@gmail.com>
2015-09-21 14:04:09 +0530

    changes in testing code

--- bonfire/basic-algorithm-scripting/12-slasher_flick.js
+++ bonfire/basic-algorithm-scripting/12-slasher_flick.js
@@ -7,3 +7,15 @@ function slasher(arr, howMany) {
 }
 
 console.log(slasher([1, 2, 3], 2));
+
+// Passing Tests
+
+//var chai = require('chai');
+//var expect = chai.expect;
+var assert = require('assert');
+
+assert.deepEqual(slasher([1, 2, 3], 2), [3], 'should drop the first two elements');
+assert.deepEqual(slasher([1, 2, 3, 4], 0), [1, 2, 3, 4], 'should return all elements when n < 1');
+assert.deepEqual(slasher([1, 2, 3], 3), [], 'should return an empty array when n >= array.length');
+assert.deepEqual(slasher([1, 2, 3], 5), [], 'should return an empty array when n >= array.length');
+
