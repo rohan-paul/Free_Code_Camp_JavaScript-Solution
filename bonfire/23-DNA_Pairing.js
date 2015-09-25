854acf2c2a2cbab3554a1e36e0fe2866da062943
Rohan Paul <rohanpaul2@gmail.com>
2015-09-25 18:19:18 +0530

    adding problem file

---
+++ bonfire/23-DNA_Pairing.js
@@ -0,0 +1,14 @@
+/* Problem - The DNA strand is missing the pairing element. Match each character with the missing element and
+return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+Remember to use RSAP if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Array.push()
+String.split()
+*/
