d0c18d6791c85c0115daffa8c224125635f312e7
Rohan Paul <rohanpaul2@gmail.com>
2015-10-05 22:18:35 +0530

    small change

--- bonfire/22-pigLatin.js
+++ bonfire/22-pigLatin.js
@@ -1,11 +1,20 @@
 /*
-Problem - Translate the provided string to pig latin.
-
-    Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
-
-    If a word begins with a vowel you just add "way" to the end.*/
-
+Problem - Translate the provided string to pig latin. Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay". If a word begins with a vowel you just add "way" to the end.*/
 
 function pigLatin(str) {
 
+    str=str.toLowerCase();
+    var n =str.search(/[aeiuo]/);
+    switch (n){
+        case 0: str = str+"way"; break;
+        case -1: str = str+"ay"; break;
+        default :
+            //str= str.substr(n)+str.substr(0,n)+"ay";
+            str=str.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+            break;
+    }
+    return str;
+
 }
+
+console.log(pigLatin("para"));
