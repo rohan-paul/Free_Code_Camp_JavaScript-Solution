c0285516ca108315594356848358ebfd4eef1668
Rohan Paul <rohanpaul2@gmail.com>
2015-11-17 07:38:41 +0530

    submitting boo_who

---
+++ bonfire/26-boo_who.js
@@ -0,0 +1,22 @@
+/* Problem -
+Check if a value is classified as a boolean primitive.*/
+
+function checkBoolean(bool) {
+    return (typeof bool === 'boolean');
+}
+
+console.log(checkBoolean(false));
+console.log(checkBoolean("JavaScript"));
+
+// Passing test
+
+var chai = require('chai');
+var expect = chai.expect;
+var assert = require('assert');
+
+assert.strictEqual(checkBoolean(true), true);
+assert.strictEqual(checkBoolean(false), true);
+assert.strictEqual(checkBoolean([1, 2, 3]), false);
+assert.strictEqual(checkBoolean({'a' : 1}), false);
+assert.strictEqual(checkBoolean(NaN), false);
+assert.strictEqual(checkBoolean('abc'), false);
