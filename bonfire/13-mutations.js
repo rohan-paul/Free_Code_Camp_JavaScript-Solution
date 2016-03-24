a4bf8b1d6d918b439d7c992c2c214d4e807ae6dc
Rohan Paul <rohanpaul2@gmail.com>
2016-03-24 22:47:32 +0530

    adding comments to explain code

--- bonfire/13-mutations.js
+++ bonfire/13-mutations.js
@@ -1,11 +1,10 @@
-/*
-Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+/* Build a function that takes an array of strings as its argument. And return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
-    For example, ['hello', 'Hello'], should return true because all of the letters in the second string are present in the first, ignoring case.
+For example, ['hello', 'Hello'], should return true because all of the letters in the second string are present in the first, ignoring case.
 
 The arguments ['hello', 'hey'] should return false because the string 'hello' does not contain a 'y'.
 
-    Lastly, ['Alien', 'line'], should return true because all of the letters in 'line' are present in 'Alien'.*/
+Lastly, ['Alien', 'line'], should return true because all of the letters in 'line' are present in 'Alien'.*/
 
 function mutation(arr) {
     for(var i = 0; i < arr[1].length; i++) {
@@ -16,7 +15,7 @@ function mutation(arr) {
     return true;
 }
 
-console.log(mutation(['hello', 'hey']));
+console.log(mutation(['hello', 'hEllo']));
 
 //Passing Tests
 
@@ -27,3 +26,8 @@ var expect = chai.expect;
 expect(mutation(['hello', 'hey'])).to.be.false;
 expect(mutation(['hello', 'Hello'])).to.be.true;
 expect(mutation(['ABCcdef', 'Dd'])).to.be.true;
+
+// My note - In the line if(arr[0].toLowerCase().indexOf(arr[1][i].toLowerCase()) <0 )
+// the string method indexOf() checks that if each of the arr[1]'s
+// elements are also present in the arr[0]
+// i.e. each of the arr[1]'s element has a non-negative index number in arr[0]
