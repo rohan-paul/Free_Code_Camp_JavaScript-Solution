4f7f68b08c050b4fc4f80c10b28600461ccf7941
Rohan Paul <rohanpaul2@gmail.com>
2018-06-20 23:59:27 +0530

    title case alternative sol

--- bonfire/06-title_case_a_sentence.js
+++ bonfire/06-title_case_a_sentence.js
@@ -38,3 +38,19 @@ expect(("I'm the little tea pot").toTitleCase()).to.be.a('String');
 expect(("I'm the little tea pot").toTitleCase()).to.equal("I'm The Little Tea Pot");
 expect(("I'm a little tea pot").toTitleCase()).to.equal("I'm A Little Tea Pot");
 expect(("GREAT MINDS DISCUSS IDEAS").toTitleCase()).to.equal("Great Minds Discuss Ideas");
+
+// Without using regexp
+
+titleCase = str => {
+
+    // Assuming there's no special character to clean up spllit the string by single space and return an array of words
+    str = str.toLowerCase().split(' ')
+
+    // Now traverse the array of words and make each word to titlecase
+    for (let i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase + str[i].slice(1);
+    }
+    return str.join(' ')
+}
+
+console.log(titleCase("I'm the little tea pot"));
