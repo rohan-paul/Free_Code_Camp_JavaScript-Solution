8cf03e75cfcc1f792178912d46fd7b1423c4281c
Rohan Paul <rohanpaul2@gmail.com>
2018-07-02 01:14:47 +0530

    roman to to decimal alt solution

--- bonfire/20-roman-To-Decimal.js
+++ bonfire/20-roman-To-Decimal.js
@@ -1,9 +1,10 @@
+// Same logic in reverse of what is applicable for converting decimal to roman. Starting from the left-most index-0 position, for each roman letter find the corresponding decimal letter > replace that letter with empty space > and then go for the next roman-letter find the next decimal number and add (a simple arithmetic addition ) it to the previous decimal number.
 
 romanToDecimal = str => {
 
+  // the result is now a number, not a string
   let decimalResult = 0;
 
-  // the result is now a number, not a string
   let decimal = [1000,  900, 500,  400,  100,  90,  50,  40,   10,  9,   5,  4,   1];
   let roman =    ["M", "CM",  "D",  "CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
 
@@ -21,6 +22,24 @@ romanToDecimal = str => {
   return decimalResult;
 }
 
-console.log(romanToDecimal("LXXXVII"));
-console.log(romanToDecimal("XLIII"));
-console.log(romanToDecimal("XXII"));
+// console.log(romanToDecimal("LXXXVII"));  // => 87
+// console.log(romanToDecimal("XLIII"));    // => 43
+// console.log(romanToDecimal("XXII"));    // => 22
+
+romanToDecimal2 = str => {
+
+  let lookup = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
+  let decimalResult = 0;
+
+  for ( let i in lookup ) {
+    while (str.indexOf(i) ===0) {
+      decimalResult += lookup[i];
+      str = str.replace(i, '')
+    }
+  }
+  return decimalResult;
+}
+
+console.log(romanToDecimal2("LXXXVII"));  // => 87
+console.log(romanToDecimal2("XLIII"));    // => 43
+console.log(romanToDecimal2("XXII"));    // => 22
