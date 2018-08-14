c3ac6e3f670586e63526b34fd32c3aff7762fe4a
Rohan Paul <rohanpaul2@gmail.com>
2018-08-15 02:33:16 +0530

    chunking an array

--- 11-chunky_monkey-2.js
+++ 11-chunky_monkey-2.js
@@ -1,4 +1,4 @@
-/*  PROBLEM - Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a multidimensional array
+/* SOLUTION-1  PROBLEM - Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a multidimensional array
 
 Create an empty array to hold the chunks called chunked_arr
 Iterate through the given array, for each element in the given array
@@ -21,7 +21,7 @@ chunk = (arr, chunkSize) => {
         const lastChunk = chunkedArr[chunkedArr.length - 1]
 
         // for the start of the array there will be no lastChunk, as I am starting with an empty array
-        if (!lastChunk || lastChunk.length === chunkSize ) {
+        if (!lastChunk || lastChunk.length === chunkSize) {
             chunkedArr.push([arr[i]]);
         } else {
             lastChunk.push(arr[i]);
@@ -45,3 +45,45 @@ let arr1 = [1, 2, 3]
 arr1.push([4])
 console.log(arr1)  // => [ 1, 2, 3, [ 4 ] ]
 */
+
+/* SOLUTION-2 - Create an empty array to hold the chunks called chunked_arr.
+Calculate the number of chunks
+Loop over the number of chunks
+Cut the array with given size and push into the chunked_arr. */
+
+chunk_2 = (arr, chunkSize) => {
+
+    let chunkedArr = []
+
+    // Count the number of chunks. i.e. the number of inner nested arrays, ie. the number of elements of the chunkedArr
+
+    let numOfChunks = Math.ceil(arr.length / chunkSize);
+
+    for (let i = 0; i < numOfChunks; i++) {
+        chunkedArr.push(arr.splice(0, chunkSize));
+    }
+    return chunkedArr
+}
+
+console.log(chunk_2([1, 2, 3, 4, 5], 2))
+
+/* SOLUTION-3 - Create an empty array to hold the chunks called chunked_arr.
+Declare a variable called index started at 0
+While index is less than length of the given array
+   Slice the array from index to size + index and push it into the chunked_arr
+   Increment the index with size */
+
+chunk_3 = (arr, chunkSize) => {
+
+    let chunkedArr = []
+    jumpIndex = 0;
+
+    while (jumpIndex < arr.length) {
+        chunkedArr.push(arr.slice(jumpIndex, (jumpIndex + chunkSize)))
+        jumpIndex += chunkSize
+    }
+
+    return chunkedArr;
+}
+
+console.log(chunk_3([1, 2, 3, 4, 5], 2))
